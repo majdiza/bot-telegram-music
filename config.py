f4c9bcabf89c8cf70e5b2059d91ce488bc095d7c
majdiza <savemeinii@gmail.com>
2025-07-25 17:56:41 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # config.py
 
 # Ganti dengan Token Bot Anda yang didapat dari @BotFather di Telegram
-TOKEN = "GANTI_DENGAN_TOKEN_BOT_ANDA"
+TOKEN = "8396193344:AAHV9MVDGE3odrCv9B5NyFbrp6iz-QrnCqk"
 
 # Ganti dengan User ID Telegram Anda (bisa lebih dari satu, pisahkan dengan koma)
 # Untuk mendapatkan ID, kirim pesan /start ke @userinfobot
-ADMIN_IDS = [123456789] 
+ADMIN_IDS = [1633716854] 
 
 # Atur batas maksimal ukuran file unduhan dalam Megabyte (MB)
 MAX_FILE_SIZE_MB = 25

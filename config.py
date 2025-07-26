95a0cc525a55b08b7561dec07dce569aedfb425c
majdiza <savemeinii@gmail.com>
2025-07-26 12:55:07 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 
 # Ganti dengan Token Bot Anda yang didapat dari @BotFather di Telegram
-TOKEN = "8396193344:AAHV9MVDGE3odrCv9B5NyFbrp6iz-QrnCqk"
+TOKEN = "8059035130:AAFKf_Kutzr_TcqaRCdwn4GPuCjJwn6D26w"
 
 # Ganti dengan User ID Telegram Anda (bisa lebih dari satu, pisahkan dengan koma)
 # Untuk mendapatkan ID, kirim pesan /start ke @userinfobot

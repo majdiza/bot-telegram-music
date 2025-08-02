ffefbb686a8ee0bb33efc76e3f83b92b59236d3c
Raisolah <108904344+Raisolah@users.noreply.github.com>
2025-08-02 15:13:08 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 
 # Ganti dengan Token Bot Anda yang didapat dari @BotFather di Telegram
-TOKEN = "8059035130:AAHVtlLvoBehF-yHUyIQmuWp6eY8gcl2798"
+TOKEN = "8396193344:AAGkcENTn04FiUPIf85iK6vbRyamkrTVa5U"
 
 # Ganti dengan User ID Telegram Anda (bisa lebih dari satu, pisahkan dengan koma)
 # Untuk mendapatkan ID, kirim pesan /start ke @userinfobot
